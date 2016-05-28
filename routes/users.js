a748ddf3648272f24099629e6e438c37bf534ae1
Yeshwanth Reddy <yeshwanth.syr@gmail.com>
2016-05-29 00:21:57 +0530

    temp route-update

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
+    "workout-name": "Workout-1",
   "Routine Type": "Weekly",
   "Monday": {
   	"Target Muscle":"Legs",
